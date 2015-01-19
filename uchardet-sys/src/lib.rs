dd30410d611412aa383cbd8f5fed8f73a2713c33
Eric Kidd <git@randomhacks.net>
2015-01-19 09:16:30 -0500

    Narrow down use of "allow(unstable)"

--- uchardet-sys/src/lib.rs
+++ uchardet-sys/src/lib.rs
@@ -1,9 +1,7 @@
 //! Low-level, unsafe wrapper for the uchardet API.  Not intended to be
 //! used directly, unless you're writing your own wrapper.
 
-#![allow(unstable)]
-
-extern crate libc;
+#[allow(unstable)] extern crate libc;
 
 use libc::{c_char, c_int, c_void, size_t};
 
